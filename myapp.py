0fcc90357d1a97e42cfde8fc681c239f13c338ea
MysteryMan6174 <Mystery_Man6174@proton.me>
2023-03-10 00:02:51 +0530

    Finalized changes for today

--- myapp.py
+++ myapp.py
@@ -3,11 +3,17 @@ from sqlalchemy import create_engine, text
 
 myapp = Flask(__name__)
 
-
 @myapp.route("/")
 def root_html():
     return render_template("home.html")
 
+@myapp.route("/blogs.html")
+def blogs_html():
+    return render_template("blogs.html")
+
+@myapp.route("/about.html")
+def about_html():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     myapp.run(debug=True)
